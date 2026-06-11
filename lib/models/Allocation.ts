026c157f0a93e723477aa5987ba4d29b31a3be65
awesomemohsin <mohsindude5@gmail.com>
2026-06-11 15:57:46 +0600

    feat: add Mongoose Allocation schema model

---
+++ lib/models/Allocation.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from 'mongoose';
+import { StatusType } from '../types';
+
+const AllocationSchema = new Schema(
+  {
+    companyId: { type: Schema.Types.ObjectId, ref: 'Company', required: true },
+    cacheProviderId: { type: Schema.Types.ObjectId, ref: 'CacheProvider', required: true },
+    serverId: { type: Schema.Types.ObjectId, ref: 'Server', required: true },
+    capacityGB: { type: Number, required: true },
+    goLiveDate: { type: Date, required: true },
+    status: { type: String, enum: Object.values(StatusType), default: StatusType.ACTIVE, required: true },
+    notes: { type: String },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: (doc, ret: any) => {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
+
+export const Allocation = mongoose.models.Allocation || mongoose.model('Allocation', AllocationSchema);
