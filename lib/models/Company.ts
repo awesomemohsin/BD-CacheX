ce65b94a86b26988d38a9eac6e216e1420d4c030
awesomemohsin <mohsindude5@gmail.com>
2026-06-11 15:57:46 +0600

    feat: add Mongoose Company schema model

---
+++ lib/models/Company.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from 'mongoose';
+import { CompanyType, StatusType } from '../types';
+
+const CompanySchema = new Schema(
+  {
+    name: { type: String, required: true },
+    type: { type: String, enum: Object.values(CompanyType), required: true },
+    email: { type: String, required: true },
+    address: { type: String },
+    status: { type: String, enum: Object.values(StatusType), default: StatusType.ACTIVE, required: true },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: (doc, ret: any) => {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
+
+export const Company = mongoose.models.Company || mongoose.model('Company', CompanySchema);
