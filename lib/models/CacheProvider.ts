c1487f567b1c724216134ac23f7e4872930e4f55
awesomemohsin <mohsindude5@gmail.com>
2026-06-11 15:57:46 +0600

    feat: add Mongoose CacheProvider schema model

---
+++ lib/models/CacheProvider.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from 'mongoose';
+import { StatusType } from '../types';
+
+const CacheProviderSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    shortCode: { type: String, required: true },
+    description: { type: String },
+    status: { type: String, enum: Object.values(StatusType), default: StatusType.ACTIVE, required: true },
+    serverCount: { type: Number, default: 0 },
+    totalCapacity: { type: Number, default: 0 },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: (doc, ret: any) => {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
+
+export const CacheProvider =
+  mongoose.models.CacheProvider || mongoose.model('CacheProvider', CacheProviderSchema);
